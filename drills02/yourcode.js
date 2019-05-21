843743c9e85a3595bd9e90f1b51af7f48c5d4650
Diana Curtis <dianacurtis92@gmail.com>
2019-05-20 17:31:19 -0700

    completed the first problem from drills 2 count the number of times a specific word occurs from an array

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,7 +1,26 @@
+/*
+Count Occurances: Given an array of words and a separate word, count how many times a particalur word given occurs within that array
+Example:
+input : ['dog','cat','dog','pig','canary','cat','dog'], 'dog'
+return 3
 
+create a container that stores the count for the word match
+go through the array from start to finish in a for loop
+    use a conditional to check if the current word in the array matches the search word
+    if T increase the count
+    if F do nothing
 
-function countOccurences(){
+    return the count
+*/
 
+function countOccurences(firstArray,searchWord){
+    let count = 0;
+    for(firstIndex=0;firstIndex<firstArray.length;firstIndex++){
+        if(firstArray[firstIndex] == searchWord){
+            count++;
+        }
+    }
+    return count;
 }
 
 function wordLengths(){
